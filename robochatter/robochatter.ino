dc7c641b55971394842af580faf76a0bbcb7689d
Don Rowe <rowedonalde@gmail.com>
2013-04-26 21:26:00 -0700

    Added the robochatter program

---
+++ robochatter/robochatter.ino
@@ -0,0 +1,23 @@
+/**
+ * robochatter.ino
+ *
+ * Cycles through 10 random notes every second
+ *
+ * Hardware:
+ * 8ohm speaker on pin 8
+ * Arduino Uno and better
+ */
+
+#include "pitches.h"
+#include "stdlib.h"
+
+int tone_output = 8;
+
+void setup() {
+}
+
+void loop(){
+  int note = rand() % NOTE_DS8;
+  tone(tone_output, note, 100);
+  delay(100);
+}
